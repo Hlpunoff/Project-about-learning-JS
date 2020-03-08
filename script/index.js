1dc0f556b6465210953298883b4ae1b4e0c34c35
Vasiliy <example@gmail.com>
2020-03-08 18:51:41 +0500

    first commit

---
+++ script/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person);
+console.log(person["name"]);
+
+let arr = ['apple.png', 'orange.jpg', 'banana.svg'];
+
+console.log(arr[2]);
+console.log(arr.length);
